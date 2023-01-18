7656b6a46c8ad2379ab493f1014eeec74b01e98a
Joe <jrbevan537@gmail.com>
2023-01-18 21:14:02 +0100

    Create fundtion to add rows

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const container = document.getElementById('container');
 let rows = document.getElementsByClassName('gridRow');
+let root = document.documentElement;
 
 function makeGrid(numberOfRows, numberOfColumns) {
-  for (i = 0; i <= numberOfRows; i++) {
+  container.style.setProperty('--row-number', numberOfRows);
+  container.style.setProperty('--column-number', numberOfColumns);
+  for (i = 0; i <= numberOfRows * numberOfColumns; i++) {
     let row = document.createElement('div');
     container.appendChild(row).className = 'gridRow';
-    
   }; 
 };
 
-makeGrid(44);
 
-console.log(rows.Length);
+makeGrid(20,20);
+
